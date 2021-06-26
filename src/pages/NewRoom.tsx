e4a135f13d8351fdbfe4747226252ddccbe6a8a7
Victor Ross <victor2ross@gmail.com>
2021-06-26 10:43:19 -0300

    Primeiro commit

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,45 @@
+import { FormEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useAuth } from '../hooks/useAuth';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  const [newRoom, setNewRoom] = useState();
+
+  function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  return(
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main className="main-content">
+        <div>
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form onSubmit={handleCreateRoom}>
+            <input 
+              type="text" 
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente?<Link to="/">Clique aqui</Link>  
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
