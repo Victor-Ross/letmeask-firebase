575c4035605ebdd2c0b4d541025476cf3aa07aef
Victor Ross <victor2ross@gmail.com>
2021-06-27 18:55:22 -0300

    Adicionado controle admin nas perguntas e like de usuários

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import { database } from '../services/firebase';
+import { useAuth } from './useAuth';
 
 
 type QuestionType = {
@@ -12,6 +13,8 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 }
 
 type FirebaseQuestions = Record<string, {
@@ -22,10 +25,15 @@ type FirebaseQuestions = Record<string, {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likes: Record<string, {
+    authorId: string;
+  }>
 }>
 
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
+
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -41,14 +49,21 @@ export function useRoom(roomId: string) {
           content: value.content,
           author: value.author,
           isHighlighted: value.isHighlighted,
-          isAnswered: value.isAnswered
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
         }
       });
 
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     });
-  }, [roomId]);
+
+    return () => {
+      roomRef.off('value');
+    }
+
+  }, [roomId, user?.id]);
 
   return { questions, title };
 }
